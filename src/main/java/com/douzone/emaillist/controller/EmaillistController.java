771e0e18d53285a61b31cce59c9f7fdbaf5e8d35
cogud0908 <cogud0908@naver.com>
2019-02-19 09:16:26 +0900

    upload 02/19

---
+++ src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -0,0 +1,51 @@
+package com.douzone.emaillist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
+
+@Controller
+public class EmaillistController {
+
+	// 자동주입
+	@Autowired
+	private EmaillistDao dao;
+	
+//	@ResponseBody
+//	@RequestMapping("")
+//	public ModelAndView list() {
+//		
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject(dao.getList());
+//		mav.setViewName("/WEB-INF/views/list.jsp");
+//		
+//		return mav;
+//	}
+	
+	@RequestMapping({"","/list"})
+	public String list(Model model) {
+		
+		model.addAttribute("list", dao.getList());
+				
+		return "/WEB-INF/views/list.jsp";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		dao.insert(vo);
+		
+		return "redirect:/";
+	}
+}
